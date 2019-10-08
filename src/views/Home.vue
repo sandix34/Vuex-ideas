0c008ed1cf885cd7e9e218e029f56192f8edd833
sandrine <sandrinezocca@gmail.com>
2019-10-08 15:47:22 +0200

    création des components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="home">
+    <h1>Welcome to Ideas.io</h1>
+    <Ideas/>
+    <IdeaCreate/>
   </div>
 </template>
 
 <script>
+import Ideas from '@/components/Ideas' ;
+import IdeaCreate from '@/components/IdeaCreate';
 
 export default {
-  name: "home"
+  name: "home",
+  components: {
+    Ideas,
+    IdeaCreate,
+  },
 };
 </script>
